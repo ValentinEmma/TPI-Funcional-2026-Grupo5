3118c5258a632ad6b9de1aa4eae7be8f4812b0f7
ValentinEmma <valentin22aguirre@gmail.com>
2026-05-29 14:43:28 -0300

    feat: implementadas funciones transicion y timer

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -16,3 +16,45 @@
 			    (print "argumentos invalidos")
 			)
 )
+
+;; ========================================================
+;; FUNCIÓN: transicion
+;; NATURALEZA: Pura
+;; ESTRATEGIA: Selectiva y Modular
+;; IMPACTO: No destructiva
+;; ========================================================
+(defun transicion (estadoActual cambiar)
+					(cond
+						((and (equal estadoActual 'en-rojo) (equal cambiar 'verde))
+								(list estadoActual "cambiar a verde"))
+
+						((and (equal estadoActual 'en-amarillo) (equal cambiar 'rojo))
+								(list estadoActual "cambiar a rojo"))
+						((and (equal estadoActual 'en-verde) (equal cambiar 'amarillo))
+								(list estadoActual "cambiar a amarillo"))
+						(t 
+							(list estadoActual "accion por defecto")
+						)
+					)
+)
+
+
+;; ========================================================
+;; FUNCIÓN: timer
+;; NATURALEZA: Pura
+;; ESTRATEGIA: Selectiva
+;; IMPACTO: No destructiva
+;; ========================================================
+(defun timer (segundos)
+				(let
+					((tiempo-ciclo (mod segundos 216)))
+
+					(cond
+						((and (>= tiempo-ciclo 0) (<= tiempo-ciclo 89)) 'ROJO)
+						((and (>= tiempo-ciclo 90) (<= tiempo-ciclo 95)) 'AMARILLO)
+						((and (>=  tiempo-ciclo 96) (<= tiempo-ciclo 215)) 'VERDE)
+						(t 'ERROR')
+					)
+				)
+)
+
