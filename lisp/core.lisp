a97f0e01be854bca8c6617d0191378c2926e91b1
ramiroaquino1 <aquinorama0000@gmail.com>
2026-06-05 21:27:41 -0300

    fix: implementacion completa Extensión 1, ciclo de 225s y correccion de solapamiento

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -1,17 +1,17 @@
 ;; ========================================================
-;; FUNCIÓN: sistema-semaforo
+;; FUNCIÓN: sistema-semaforo-ext-1
 ;; NATURALEZA: Impura (realiza impresiones en pantalla)
 ;; ESTRATEGIA: Selectiva y Modular
 ;; IMPACTO: No destructiva
 ;; ========================================================
 
-(defun sistema-semaforo (estadoActual cambiar segundos)
+(defun sistema-semaforo-ext-1 (estadoActual cambiar segundos)
 			(if (and (symbolp estadoActual)(symbolp cambiar) (numberp segundos))
 
 			    (progn
 			        (print "=== SISTEMA DE SEMAFOROS ===")
-			        (print (transicion estadoActual cambiar))
-			        (print (timer segundos))
+			        (print (transicion-ext-1 estadoActual cambiar))
+			        (print (timer-ext-1 segundos))
 			        (print (logging-auditoria segundos estadoActual cambiar))
 			    )
 			    (print "argumentos invalidos")
@@ -25,7 +25,7 @@
 ;; IMPACTO: No destructiva
 ;; ========================================================
 
-(defun transicion (estadoActual cambiar)
+(defun transicion-ext-1 (estadoActual cambiar)
   (cond 
     ( (not (and (symbolp estadoActual) (symbolp cambiar)))
     	(pprint "error en los datos ingresados"))
@@ -56,29 +56,20 @@
 
 
 ;; Caso normal
-;(transicion 'en-rojo 'intermitente)
-
-;; Resultado esperado:
-;; (EN-ROJO "cambiar a intermitente")
+;(transicion-ext-1 'en-rojo 'intermitente)
+;; Resultado esperado: (EN-ROJO "cambiar a intermitente")
 
 ;; Caso normal
-;(transicion 'intermitente 'amarillo)
-
-;; Resultado esperado:
-;; (EN-intermitente "cambiar a amarillo")
+;(transicion-ext-1 'intermitente 'en-amarillo)
+;; Resultado esperado: (INTERMITENTE "cambiar a amarillo")
 
 ;; Caso alternativo
-;(transicion 'en-rojo 'amarillo)
-
-;; Resultado esperado:
-;; (EN-ROJO "accion por defecto")
+;(transicion-ext-1 'en-rojo 'en-amarillo)
+;; Resultado esperado: (EN-ROJO "accion por defecto")
 
 ;; Caso de error
-;(transicion 10 'verde)
-
-;; Resultado esperado:
-;; ERROR
-
+;(transicion-ext-1 10 'en-verde)
+;; Resultado esperado: "error en los datos ingresados"
 
 ;; ========================================================
 ;; FUNCIÓN: timer-ext-1
@@ -87,8 +78,8 @@
 ;; IMPACTO: No destructiva
 ;; ========================================================
 
-(defun timer (segundos)
-  (let ((tiempo-ciclo (mod segundos 222)))
+(defun timer-ext-1 (segundos)
+  (let ((tiempo-ciclo (mod segundos 225)))
 
     (cond
       ((and (>= tiempo-ciclo 0)(<= tiempo-ciclo 89))'ROJO)
@@ -96,72 +87,33 @@
       ((and (>= tiempo-ciclo 93)(<= tiempo-ciclo 98))'AMARILLO)
       ((and (>= tiempo-ciclo 99)(<= tiempo-ciclo 101))'INTERMITENTE)
       ((and (>= tiempo-ciclo 102)(<= tiempo-ciclo 221))'VERDE)
+      ((and (>= tiempo-ciclo 222) (<= tiempo-ciclo 224)) 'INTERMITENTE)
       (t 'ERROR))))
 
 
+;; Caso normal
+;(timer-ext-1 50)
+;; Resultado esperado: ROJO
 
-(defun timer (segundos)
-				(let
-					((tiempo-ciclo (mod segundos 222)))
-
-					(cond
-						((and (>= tiempo-ciclo 0) (<= tiempo-ciclo 89)) 'ROJO)
-						((and (>= tiempo-ciclo 90) (<= tiempo-ciclo 95)) 'AMARILLO)
-						((and (>=  tiempo-ciclo 96) (<= tiempo-ciclo 215)) 'VERDE)
-						(t 'ERROR)
-					)
-				)
-)
-
+;; Caso normal (Cae justo en la primera intermitencia)
+;(timer-ext-1 91)
+;; Resultado esperado: INTERMITENTE
 
 ;; Caso normal
-;(timer 50)
-
-;; Resultado esperado:
-;; ROJO
-
+;(timer-ext-1 95)
+;; Resultado esperado: AMARILLO
 
 ;; Caso normal
-;(timer 91)
+;(timer-ext-1 150)
+;; Resultado esperado: VERDE
 
-;; Resultado esperado:
-;; INTERMITENTE
-
-
-;; Caso normal
-;(timer 95)
-
-;; Resultado esperado:
-;; AMARILLO
-
-
-;; Caso normal
-;(timer 100)
-
-;; Resultado esperado:
-;; INTERMITENTE
-
-
-;; Caso normal
-;(timer 150)
-
-;; Resultado esperado:
-;; VERDE
-
-
-;; Caso alternativo
-;; Comienza un nuevo ciclo
-;(timer 222)
-
-;; Resultado esperado:
-;; ROJO
-
+;; Caso alternativo (Comienza un nuevo ciclo exacto)
+;(timer-ext-1 225)
+;; Resultado esperado: ROJO
 
 ;; Caso de error
-;(timer 'hola)
-
-;; Resultado esperado:
-;; ERROR
+;(timer-ext-1 'hola)
+;; Resultado esperado: ERROR
 
 
 ;; ========================================================
@@ -209,50 +161,42 @@
 ;; de datos existente)
 ;; ========================================================
 
-(defun duracion-ciclo(rojo intermitente amarillo verde)
-  (cond
-    (
-    	(not (and (integerp intermitente) (integerp rojo)(integerp amarillo)(integerp verde)) )
-     "Error: uno de los parametros no es un numero entero"
-    )
-    (t
-     (let ((tiempo-un-ciclo (+ intermitente rojo amarillo verde intermitente)) )
-       (format t "El ciclo dura ~a's~%" tiempo-un-ciclo)
-       ;;se retorna el valor calculado 
-       ;;por si es necesaria su utilizacion posteriormente
-       tiempo-un-ciclo
-      )
-    )
+
+  (defun duracion-ciclo-ext-1 (rojo amarillo verde intermitente)
+  (if (and (integerp rojo) (integerp amarillo) (integerp verde) (integerp intermitente))
+      (let ((tiempo-un-ciclo (+ rojo amarillo verde (* intermitente 3)))) ; 
+        (format t "El ciclo dura ~a segundos~%" tiempo-un-ciclo)
+        tiempo-un-ciclo)
+      "Error: uno de los parametros no es un numero entero")
   )
-)
 
 ;; ==========================
 ;; CASOS DE PRUEBA
 ;; ==========================
+;; Caso normal (90s rojo, 6s amarillo, 120s verde, 3s intermitencia)
+;(duracion-ciclo-ext-1 90 6 120 3)
+;; Resultado esperado: "El ciclo dura 225 segundos" y devuelve 225.
 
-;;caso normal
-;(duracion-ciclo 6 30 10 60)
+;; Caso alternativo / Error (se ingresa un decimal)
+;(duracion-ciclo-ext-1 90.5 6 120 3)
+;; Resultado esperado: "Error: uno de los parametros no es un numero entero"
 
-;;caso normal
-
-;;caso error
-;(duracion-ciclo 7 1.2 20 1.2)
-
-;;caso error
-;(duracion-ciclo 'siete 'veinte 'cuarenta 'cincuenta)
+;; Caso de error (se ingresan palabras)
+;(duracion-ciclo-ext-1 'noventa 'seis 'ciento-veinte 'tres)
+;; Resultado esperado: "Error: uno de los parametros no es un numero entero"
 
 
 
 
 ;; ========================================================
-;; FUNCIÓN: recomendacion-ciclo 
+;; FUNCIÓN: recomendacion-ciclo -ext-1
 ;; NATURALEZA: Impura (realiza salida por pantalla)
 ;; ESTRATEGIA: Función simple (implementada mediante cond)
 ;; IMPACTO: No destructiva (no modifica estructuras de
 ;; datos existentes)
 ;; ========================================================
 
-(defun recomendacion-ciclo(total-ciclo)
+(defun recomendacion-ciclo-ext-1(total-ciclo)
 	(cond 
 		((> 35 total-ciclo) "recomendacion: aumentar el tiempo del ciclo para obtener entre 35 a 150 segundos")
 		((and (>= total-ciclo 35) (<= 150 total-ciclo)) "¡su tiempo esta en los estandares optimos!" )
@@ -265,16 +209,16 @@
 ;; ==========================
 
 ;;caso normal
-;(recomendacion-ciclo 120)
+;(recomendacion-ciclo-ext-1 120)
 
 
-;;caso de ciclo demasiado corto
-;(recomendacion-ciclo 20)
+;; Caso alternativo (ciclo demasiado corto)
+;(recomendacion-ciclo-ext-1 20)
+;; Resultado esperado: "recomendacion: aumentar el tiempo del ciclo para obtener entre 35 a 150 segundos"
 
-
-;; Caso de ciclo demasiado largo
-;(recomendacion-ciclo 160)
-
+;; Caso alternativo (ciclo demasiado largo)
+;(recomendacion-ciclo-ext-1 225)
+;; Resultado esperado: "recomendacion: disminuya su tiempo para obtener un ciclo entre 35 a 150 segundos"
 
 
 ;;caso error excepcion
@@ -290,28 +234,18 @@
 ;; IMPACTO: No destructiva
 ;; ========================================================
 
-(defun ciclos-por-tiempo (minutos)
+(defun ciclos-por-tiempo-ext-1 (minutos)
     (if (numberp minutos)
         (floor
-            (/ (* minutos 60) 222)
-        )
-        'ERROR
-    )
-)
-
-
-(defun ciclos-por-tiempo (minutos)
-    (if (numberp minutos)
-        (floor
-            (/ (* minutos 60) 216)
+            (/ (* minutos 60) 225)
         )
         'ERROR
     )
 )
 ;; Caso normal
 ;; 15 minutos = 900 segundos
-;; 900 / 216 = 4 ciclos completos
-;(ciclos-por-tiempo 15)
+;; 900 / 225 = 4 ciclos exactos
+;(ciclos-por-tiempo-ext-1 15)
 
 ;; Resultado esperado:
 ;; 4
@@ -319,41 +253,43 @@
 ;; Caso alternativo
 ;; Tiempo menor a un ciclo completo
 ;; 3 minutos = 180 segundos
-;(ciclos-por-tiempo 3)
+;(ciclos-por-tiempo-ext-1 3)
 
 ;; Resultado esperado:
 ;; 0
 
 ;; Caso de error
 ;; Se ingresa un símbolo
-;(ciclos-por-tiempo 'hola)
+;(ciclos-por-tiempo-ext-1 'hola)
 
 ;; Resultado esperado:
 ;; ERROR
 
 ;; ========================================================
-;; FUNCIÓN: informe-distribucion-60min
+;; FUNCIÓN: informe-distribucion-60min-ext-1
 ;; NATURALEZA: Pura (Realiza cálculos matemáticos sin efectos secundarios)
 ;; ESTRATEGIA: Modular (Llamada a funciones de apoyo)
 ;; IMPACTO: No destructiva
 ;; ========================================================
-(defun informe-distribucion-60min ()
-	  (let ((ciclo-total (+ 90 6 120))) ; Duración de un ciclo completo (216s)
+(defun informe-distribucion-60min-ext-1 ()
+	  (let ((ciclo-total (+ 90 6 120 9))) ; Duración de un ciclo completo (225)
 	    (list 
 	      (list 'ROJO     (calcular-porcentaje 90 ciclo-total))
 	      (list 'AMARILLO (calcular-porcentaje 6 ciclo-total))
 	      (list 'VERDE    (calcular-porcentaje 120 ciclo-total))
+          (list 'INTERMITENTE (calcular-porcentaje 9 ciclo-total)))
 		)
 	 )
-)
+
 
 ;; Caso normal
-;(informe-distribucion-60min)
+;(informe-distribucion-60min-ext-1)
 
 ;; Resultado esperado:
-;; ((ROJO 41.67)
-;;  (AMARILLO 2.78)
-;;  (VERDE 55.56))
+;; ((ROJO 40.0)
+;;  (AMARILLO 2.67)
+;;  (VERDE 53.33)
+;;  (INTERMITENTE 4.0))
 
 ;; Caso alternativo
 ;; Ejecutar varias veces para verificar que siempre
@@ -361,9 +297,10 @@
 ;(informe-distribucion-60min)
 
 ;; Resultado esperado:
-;; ((ROJO 41.67)
-;;  (AMARILLO 2.78)
-;;  (VERDE 55.56))
+;; ((ROJO 40.0)
+;;  (AMARILLO 2.67)
+;;  (VERDE 53.33)
+;;  (INTERMITENTE 4.0))
 
 ;; ========================================================
 ;; FUNCIÓN: calcular-porcentaje
@@ -374,3 +311,5 @@
 (defun calcular-porcentaje (tiempo-color tiempo-ciclo)
   (let ((resultado (* (/ tiempo-color (float tiempo-ciclo)) 100)))
     (/ (round (* resultado 100)) 100.0)))
+   
+
